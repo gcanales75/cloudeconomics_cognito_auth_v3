3a61539aa1f045b5adafa66a62ca97be2a46432c
gilberto canales <glcnl@amazon.com>
2023-01-06 19:43:28 -0700

    add cognito config

--- js/app.js
+++ js/app.js
@@ -8,43 +8,6 @@ ClientId : clientId
 
 var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
 
-function login(){
-    var username = $('#username').val();
-    var authenticationData = {
-        Username: username,
-        Password: $('#password').val()
-    };
-
-    // checking code
-    console.log("Username:",username, "Password:",$('#password').val())
-
-    var authenticationDetails = new AmazonCognitoIdentity.AuthenticationDetails(authenticationData);
-
-    var userData = {
-        Username : username,
-        Pool : userPool
-    };
-    var cognitoUser = new AmazonCognitoIdentity.CognitoUser(userData);
-
-    console.log(cognitoUser)
-    cognitoUser.authenticateUser(authenticationDetails, {
-        onSuccess: function (result) {
-            var accessToken = result.getAccessToken().getJwtToken();
-            // Use the idToken for Logins Map when Federating User Pools with identity pools or when passing through an Authorization Header to an API Gateway Authorizer
-            // var idToken = result.idToken.jwtToken;
-
-            console.log("Authentication successful", accessToken)
-            window.location = './index.html'
-        },
-
-        onFailure: function(err) {
-            console.log("failed to authenticate");
-            console.log(JSON.stringify(err))
-            alert("Failed to Log in.\nPlease check your credentials.")
-        },
-    });
-}
-
 function checkLogin(redirectOnRec, redirectOnUnrec){
 
     var cognitoUser = userPool.getCurrentUser();
