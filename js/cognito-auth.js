5646e031f90305c0390edb65f82bf2c19380ba78
gilberto canales <glcnl@amazon.com>
2023-01-07 17:57:40 -0700

    add cognito config

--- js/cognito-auth.js
+++ js/cognito-auth.js
@@ -3,21 +3,32 @@
 var userPoolId = 'us-east-1_URs9xI1kz'
 var clientId = 'ei5gne42deoph24kkvaucp5mq'
 var CognitoUserPool = AmazonCognitoIdentity.CognitoUserPool;
+var USER_POOL_ID = 'us-east-1_URs9xI1kz'
+var APP_CLIENT_ID = 'ei5gne42deoph24kkvaucp5mq'
 
 console.log('Loading Cognito auth...');
 
 var poolData = { UserPoolId : userPoolId,
   ClientId : clientId
-  };
+};
   
   console.log(poolData)
 
   var userPool = new AmazonCognitoIdentity.CognitoUserPool(poolData);
-  var cognitoUser = userPool.getCurrentUser();
-  var cognitoUser = userPool.getCurrentUser();
+  var currentUser = userPool.getCurrentUser();
   
   console.log(userPool)
-  console.log(cognitoUser)
+
+function getCurrentUser() {
+  const userPool = new CognitoUserPool({
+    UserPoolId: config.cognito.USER_POOL_ID,
+    ClientId: config.cognito.APP_CLIENT_ID
+  });
+  return userPool.getCurrentUser();
+}
+
+console.log(getCurrentUser)
+
 
 /*
   function login(){
@@ -59,19 +70,19 @@ var poolData = { UserPoolId : userPoolId,
   console.log(userPool)
 
 */
-  function checkLogin(redirectOnRec, redirectOnUnrec){
-  
-      var cognitoUser = userPool.getCurrentUser();
-      if (cognitoUser != null) {
-          console.log("user exists")
-          if (redirectOnRec) {
-              window.location = './index.html';
-          } else {
-              $("#body").css({'visibility':'visible'});           
-          }
-      } else {
-          if (redirectOnUnrec) {
-              window.location = 'https://cloudeconomics.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=ei5gne42deoph24kkvaucp5mq&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fmain.d1vyhq931i0cwf.amplifyapp.com%2F'
-          } 
-      }
-  }
+function checkLogin(redirectOnRec, redirectOnUnrec){
+
+    var cognitoUser = userPool.getCurrentUser();
+    if (cognitoUser != null) {
+        console.log("user exists")
+        if (redirectOnRec) {
+            window.location = './index.html';
+        } else {
+            $("#body").css({'visibility':'visible'});           
+        }
+    } else {
+        if (redirectOnUnrec) {
+            window.location = 'https://cloudeconomics.auth.us-east-1.amazoncognito.com/oauth2/authorize?client_id=ei5gne42deoph24kkvaucp5mq&response_type=code&scope=email+openid+phone&redirect_uri=https%3A%2F%2Fmain.d1vyhq931i0cwf.amplifyapp.com%2F'
+        } 
+    }
+}
